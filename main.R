8d0643ed9852d3f796b6a0d5c1531fd6ca3d202d
Adam Labadorf <alabadorf@gmail.com>
2022-01-15 13:16:36 -0500

    added hw1 material from main repo

--- main.R
+++ main.R
@@ -1,188 +1,184 @@
-#!/usr/bin/Rscript
-## Author: Taylor Falk
-## tfalk@bu.edu
-## BU BF591
-## Assignment Week 2
+library(tibble)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(stringr)
+# library(purrr)
 
-#### Bioconductor ####
-# it is standard among R packages to define libraries and packages at the 
-# beginning of a script. Also note that a package should NOT be installed every 
-# time a script runs.
-# The bioconductor repository has installation instructions for biomaRt: 
-# https://bioconductor.org/install/
 
-if (!require("BiocManager", quietly = TRUE)){
-  install.packages("BiocManager")
-}
-if (!require("biomaRt", quietly = TRUE)){
-  BiocManager::install("biomaRt")
-}
-library(biomaRt)
-library(tidyverse)
+# ----------------------- Helper Functions to Implement ------------------------
 
-#### Tibbles tables and pipes ####
-# TODO: write a brief function to compare and contrast tibbles and DFs, use pipes
-
-#### Loading and processing data ####
-#' Load Expression Data
+#' Read the expression data "csv" file.
 #'
-#' @param filepath A text st1pm on Thursdayring of the full filepath to the file to load.
+#' Function to read microarray expression data stored in a csv file. The
+#' function should return a sample x gene tibble, with an extra column named
+#' "subject_id" that contains the geo accession ids for each subject.
 #'
-#' @return A tibble containing the data loaded from the CSV in `filepath`. 
+#' @param filename (str): the file to read.
+#'
+#' @return
+#' @export
+#'
+#' @examples expr_mat <- read_expression_table('example_intensity_data.csv')
+read_expression_table <- function(filename) {
+  expr_mat <- tibble::as_tibble(t(read.table(
+    filename,
+    header = TRUE,
+    sep = " "
+  )),
+  rownames = "subject_id")
+  return (expr_mat)
+}
+
+
+#' Replaces all '.' in a string with '_'
+#'
+#' @param str String to operate upon.
+#'
+#' @return reformatted string.
+#' @export
+#'
+#' @examples
+#' period_to_underscore("foo.bar")
+#' "foo_bar"
+period_to_underscore <- function(str) {
+  return (stringr::str_replace_all(str, "\\.", "_"))
+}
+
+
+# rename variables:
+# Age_at_diagnosis to Age
+# SixSubtypesClassification to Subtype
+# normalizationcombatbatch to Batch
+
+#' Rename and select specified columns.
+#'
+#' Function to rename Age_at_diagnosis, SixSubtypesClassification, and
+#' normalizationcombatbatch columns to Age, Subtype, and Batch, respectively. A
+#' subset of the data should be returned, containing only the Sex, Age, TNM_Stage,
+#' Tumor_Location, geo_accession, KRAS_Mutation, Subtype, and Batch columns.
+#'
+#' @param data (tibble) metadata information for each sample
+#'
+#' @return (tibble) renamed and subsetted tibble
+#' @export
+#'
+#' @examples rename_and_select(metadata)
 #' 
-#' @details Note that not all CSVs are created equal, and there are often cases where 
-#' the data will not load in correctly on the first try. You may want to write this functon to 
-#' adjust the CSV file being loaded into this assignment so that it can be formed into a 
-#' tibble correctly.
-#'
-#' @examples 
-#' `data <- load_expression('/project/bf528/project_1/data/example_intensity_data.csv')`
-load_expression <- function(filepath) {
-  tmp_csv <- read.csv(filepath, header = T, sep = " ")
-  tmp_csv <- cbind(probeids = row.names(tmp_csv), tmp_csv)
-  return(tibble(tmp_csv))
-}
-
-#' Filter 15% of the gene expression values.
-#'
-#' @param tibble A tibble of expression values, rows by probe and columns by sample.
-#'
-#' @return A tibble of affymetrix probe names from the input expression data tibble. 
-#' These names match the rows with 15% or more of the expression values about log2(15).
 #' 
-#' @details This is similar to the filters being implemented in BF528's project 1. 
-#' We do not necessarily want to capture all parts of the assay in our analysis, so 
-#' filters like this serve to reduce the noise and amount of data to examine.
-#'
-#' @examples `samples <- filter_15(data_tib)`
-#' `> str(samples)`
-#' `tibble [40,158 × 1] (S3: tbl_df/tbl/data.frame)`
-#' `$ probeids: chr [1:40158] "1007_s_at" "1053_at" "117_at" "121_at" ...`
-filter_15 <- function(tibble){
-  # for each gene, at least 15% of the gene-expression values must be > log2(15)
-  percent_gt <- function(row) {
-    # functions can be defined inside other functions, which can be one style to
-    # make your code more repeatable
-    boolean_row <- row > log2(15)
-    percent_row <- sum(boolean_row)/length(row)
-    return(percent_row)
-  }
-  # there are many ways in R to apply our function to the entire tibble
-  # fastest would be an lapply, but a for loop would work (just slowly)
-  row_pct <- apply(tibble[2:ncol(tibble)], percent_gt, MARGIN = 1) # don't capture first row
-  boolean_rows <- which(row_pct > 0.15)
-  return(tibble[boolean_rows, 1])
+rename_and_select <- function(data) {
+  out <- dplyr::rename(data,
+                       "Age" = Age_at_diagnosis, 
+                       "Subtype" = SixSubtypesClassification,
+                       "Batch" = normalizationcombatbatch) %>%
+    dplyr::select(
+      c(
+        "Sex",
+        "Age",
+        "TNM_Stage",
+        "Tumor_Location",
+        "geo_accession",
+        "KRAS_Mutation",
+        "Subtype",
+        "Batch"
+      )
+    )
+  return (out)
 }
 
-#### Gene name conversion ####
 
-#' Convert affymetrix array names into hgnc_symbol IDs using biomaRt. Inputs and 
-#' outputs will likely not be the same size.
+#' Create new "Stage" column containing "stage " prefix.
 #'
-#' @param affy_tib A single column tibble of strings containing array names.
+#' Creates a new column "Stage" with elements following a "stage x" format, where
+#' `x` is the cancer stage data held in the existing TNM_Stage column. Stage
+#' should have a factor data type.
 #'
-#' @return A 2 column tibble that contains affy IDs in the first column,
-#' and their corresponding HGNC gene ID in the second column. Note that not all affy IDs 
-#' will necessarily correspond with a gene ID, and one gene may have multiple affy IDs.
-#' 
-#' @details Connecting to ensembl via biomaRt can be...hit or miss...so you may 
-#' want to check if data was correctly returned (or if it was just empty). The 
-#' `getBM()` function may not accept a tibble, so you might need to convert your 
-#' input into a flat vector.
+#' @param data  (tibble) metadata information for each sample
 #'
-#' @examples 
-#' `> affy_to_hgnc(tibble(c('202860_at', '1553551_s_at')))`
-#' `affy_hg_u133_plus_2 hgnc_symbol`
-#' `1        1553551_s_at      MT-ND1`
-#' `2        1553551_s_at       MT-TI`
-#' `3        1553551_s_at       MT-TM`
-#' `4        1553551_s_at      MT-ND2`
-#' `5           202860_at     DENND4B`
-affy_to_hgnc <- function(affy_vector) {
-  affy_vector <- pull(affy_vector)
-  usemart <- useMart(host = 'https://www.ensembl.org',
-                     biomart = 'ENSEMBL_MART_ENSEMBL')
-  ensembl <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
-  newNames <- getBM(attributes = c('affy_hg_u133_plus_2', 'hgnc_symbol'), 
-        filters = c('affy_hg_u133_plus_2'),
-        values = affy_vector, 
-        mart = ensembl)
-  return(as_tibble(newNames))
+#' @return (tibble) updated metadata with "Stage" column
+#' @export
+#'
+#' @examples metadata <- stage_as_factor(metadata)
+stage_as_factor <- function(data) {
+  out <-
+    dplyr::mutate(data, Stage = as.factor(paste0("stage ", TNM_Stage)))
+  return (out)
 }
 
-#### ggplot ####
 
-#' Reduce a tibble of expression data to only the rows in good_genes or bad_genes.
+#' Calculate age of samples from a specified sex.
 #'
-#' @param expr_tibble A tibble holding the expression data, each row corresponding to
-#' one affymetrix probe ID and each column to a sample.
-#' @param names_ids A two column tibble that associates affy IDs with HGNC gene IDs. 
-#' Generated `with affy_to_hgnc()`.
-#' @param good_genes A list of gene names stored as a vector of strings.
-#' @param bad_genes A list of gene names stored as a vector of strings.
+#' @param data (tibble) metadata information for each sample
+#' @param sex (str) which sex to calculate mean age. Possible values are "M"
+#' and "F"
 #'
-#' @return A tibble with two additional columns added:
-#' 1. HGNC gene IDs 
-#' 2. Does the gene is this row fall into "good" or "bad" genes?
-#' This tibble should be reduced to only rows present in good or bad genes. All
-#' other rows can be discarded.
-#' 
-#' @details In order to plot only our genes of interest, we need to rearrange our 
-#' data to include only the elements we want to see. We also want to add to columns, 
-#' one that associates the probeids with the HGNC gene name, and one that says if 
-#' that gene is in the good or bad sets of genes.
+#' @return (float) mean age of specified samples
+#' @export
 #'
-#' @examples 
-#' `plot_tibble <- reduce_data(expr_tibble = expr, names_ids = sample_names,`
-#' `                           goodGenes, badGenes)`
-#' `> head(plot_tibble)`
-#' `A tibble: 6 × 38`
-#' `  probeids    hgnc    gene_set    GSM972389 ...`
-#' `  <chr>       <chr>   <chr>       <dbl>     ...`
-#' `1 202860_at   DENND4B good        7.16      ...`
-#' `2 204340_at   TMEM187 good        6.40      ...`
-reduce_data <- function(expr_tibble, names_ids, good_genes, bad_genes){
-  expr_tibble <- add_column(expr_tibble, 
-                            .after = "probeids",
-                            names_ids[match(expr_tibble$probeids,
-                                            names_ids$affy_hg_u133_plus_2), 2])
-  good_tib <- expr_tibble[which(expr_tibble$hgnc_symbol %in% good_genes),]
-  good_tib <- add_column(good_tib, .after = "hgnc_symbol", gene_set = "good")
-  bad_tib <- expr_tibble[which(expr_tibble$hgnc_symbol %in% bad_genes),]
-  bad_tib <- add_column(bad_tib, .after = "hgnc_symbol", gene_set = "bad")
-  return(rbind(good_tib, bad_tib))
+#' @examples mean_age_by_sex(metadata, "F")
+mean_age_by_sex <- function(data, sex) {
+  out <- dplyr::filter(data, Sex == sex) %>%
+    summarize(mean(Age))
+  return (out)
 }
 
-#' Plot a boxplot of good and bad genes.
+
+#' Calculate average age of samples within each cancer stage. Stages should be
+#' from the newly created "Stage" column.
 #'
-#' @param tibble A reduced tibble of expression data, with information about
-#' good and bad genes and gene names.
+#' @param data (tibble) metadata information for each sample
 #'
-#' @return A ggplot object which contains a boxplot of the genes and samples we 
-#' are interested in.
-#' 
-#' @details This function performs one additional step before using `ggplot()`: 
-#' converting the _wide_ format of the input tibble to a _long_ format.
+#' @return (tibble) summarized tibble containing average age for all samples from
+#' each stage.
+#' @export
 #'
-#' @examples `p <- plot_ggplot(plot_tibble)`
-plot_ggplot <- function(tibble) {
-  tibble %>% pivot_longer(starts_with('GSM'), names_to = 'sample') -> tibble
-  p <- ggplot(tibble, aes(x=hgnc_symbol, y=value)) +
-    geom_boxplot()
-  return(p)
+#' @examples age_by_stage(data)
+age_by_stage <- function(data) {
+  out <- dplyr::group_by(data, Stage) %>%
+    dplyr::summarise((mean(Age)))
+  return (out)
 }
 
-# this if statement means that this segment of code will only run when in 
-# interactive mode, i.e. RStudio. Using Rscript on the command line will not run 
-# this segment. 
-if(interactive()) {
-  # expr <- load_expression("/project/bf528/project_1/data/example_intensity_data.csv")
-  # samples <- filter_15(expr)
-  # sample_names <- affy_to_hgnc(samples)
-  # all_names <- sample_names$hgnc_symbol[-which(sample_names$hgnc_symbol == "")]
-  # goodGenes <- sample(all_names, 10)
-  # badGenes <- sample(all_names, 10)
-  # plot_tibble <- reduce_data(expr_tibble = expr, names_ids = sample_names,
-  #                            goodGenes, badGenes)
-  # p <- plot_ggplot(plot_tibble)
+#' Create a cross tabulated table for Subtype and Stage using dplyr methods.
+#'
+#' @param data (tibble) metadata information for each sample
+#'
+#' @return (tibble) table where rows are the cancer stage of each sample, and the
+#' columns are each cancer subtype. Elements represent the number of samples from
+#' the corresponding stage and subtype. If no instances of a specific pair are
+#' observed, a zero entry is expected.
+#' @export
+#'
+#' @examples cross_tab <- dplyr_cross_tab(metadata)
+subtype_stage_cross_tab <- function(data) {
+  out <- dplyr::group_by(data, Subtype, Stage) %>%
+    dplyr::summarise("n" = dplyr::n()) %>%
+    tidyr::pivot_wider(
+      names_from = Subtype,
+      values_from = n,
+      values_fill = 0
+    )
+  return (out)
+}
+
+#' Summarize average expression and probe variability over expression matrix.
+#'
+#' @param exprs An (n x p) expression matrix, where n is the number of samples,
+#' and p is the number of probes.
+#'
+#' @return A summarized tibble containing `main_exp`, `variance`, and `probe`
+#' columns documenting average expression, probe variability, and probe ids,
+#' respectively.
+summarize_expression <- function(exprs) {
+  probe_avg <-
+    apply(dplyr::select(expr_mat, !c("subject_id")), 2, mean)
+  probe_var <-
+    apply(dplyr::select(expr_mat, !c("subject_id")), 2, var)
+  
+  summarized <- tibble::tibble(
+    "mean_exp" = probe_avg,
+    "variance" = probe_var,
+    "probe" = names(probe_avg)
+  )
+  return (summarized)
 }
